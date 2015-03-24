271d57dd4f2d80b57a8b1cae06c43dcf26693b43
Seth Broweleit <setbro@gmail.com>
2015-03-24 14:22:39 -0500

    v1

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularfullcalendarApp
+ * @description
+ * # angularfullcalendarApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularfullcalendarApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.calendar'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/myroute', {
+        templateUrl: 'views/myroute.html',
+        controller: 'MyrouteCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularfullcalendarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularfullcalendarApp
+ */
+angular.module('angularfullcalendarApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
